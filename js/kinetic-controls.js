2a558f97a591ea3a0d32604b80ddf06d4da1453c
Martin Naumann <martin@geekonaut.de>
2014-12-19 11:16:54 +0100

    Initial commit

---
+++ js/kinetic-controls.js
@@ -0,0 +1,91 @@
+module.exports = (function() {
+  var Hammer = require('hammerjs');
+
+  var instance = {}, hammertime, cam, camAnchor, minZ = 0;
+  var swingX = 0, swingY = 0, wasMoved = false;
+
+  instance.init = function(camera, cameraAnchor, minCamZ, invertX, invertY) {
+    var hammertime = new Hammer(document.body, {});
+
+    cam = camera;
+    camAnchor = cameraAnchor;
+    minZ = minCamZ;
+
+    cam.rotation.order = 'YXZ';
+
+    hammertime.get('pinch').set({ enable: true });
+
+    hammertime.on('pan', function(e) {
+      var factor = e.pointerType === 'mouse' ? 0.01 : 0.01;
+      var turnY = (invertY ? -1 : 1) * Math.PI * factor * (e.deltaX / window.innerWidth),
+          turnX = (invertX ? -1 : 1) * Math.PI * factor * (e.deltaY / window.innerHeight);
+
+      if(camAnchor) {
+        camAnchor.rotation.y += turnY;
+        camAnchor.rotation.x += turnX;
+      } else {
+        cam.rotation.y += turnY;
+        cam.rotation.x += turnX;
+      }
+      swingX = turnX;
+      swingY = turnY;
+
+      wasMoved = true;
+      e.stopPropagation();
+      if(e.preventDefault) e.preventDefault();
+    });
+
+
+    hammertime.on('pinchmove', function(e) {
+      if(e.scale >= 1.0 && camera.position.z <= minZ) return;
+
+      camera.translateZ((1 - e.scale) * 5);
+      wasMoved = true;
+
+      e.stopPropagation();
+      if(e.preventDefault) e.preventDefault();
+    });
+
+    window.addEventListener('wheel', function(e) {
+      if(e.wheelDelta) { // Chrome
+        camera.translateZ(-(e.wheelDelta / 60));
+      } else { // IE / Firefox
+        camera.translateZ(-1 * Math.max(-10, Math.min(e.deltaY, 10)));
+      }
+      wasMoved = true;
+
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  };
+
+  instance.update = function() {
+    if(!camAnchor) camAnchor = cam;
+
+    if(swingX != 0) {
+      camAnchor.rotation.x += swingX;
+      if(swingX < -0.0001) {
+        swingX += 0.0001;
+      } else if(swingX > 0.0001) {
+        swingX -= 0.0001;
+      } else {
+        swingX = 0;
+      }
+    }
+
+    if(swingY != 0) {
+      camAnchor.rotation.y += swingY;
+      if(swingY < -0.0001) {
+        swingY += 0.0001;
+      } else if(swingY > 0.0001) {
+        swingY -= 0.0001;
+      } else {
+        swingY = 0;
+      }
+    }
+  };
+
+  instance.wasMoved = function() { return wasMoved; };
+
+  return instance;
+})();
---
+++ js/main.js
@@ -0,0 +1,115 @@
+var THREE    = require('three'),
+    World    = require('three-world'),
+    Controls = require('./kinetic-controls');
+
+module.exports = (function() {
+  var instance = {}, onRender, speed = 0.005, earth;
+
+  // Internals
+
+  var earthGeo = new THREE.SphereGeometry(600, 64, 64),
+      light  = new THREE.DirectionalLight(0x888888, 3.5, 500 ),
+      light2 = new THREE.DirectionalLight(0x888888, 3.5, 500 ),
+      light3 = new THREE.DirectionalLight(0x888888, 3.5, 500 ),
+      marker = new THREE.BoxGeometry(5, 5, 1),
+      origin = new THREE.Vector3(0, 0, 0),
+      anchor = new THREE.Object3D();
+
+  light2.position.set(600, 0, 0);
+  light2.target.position.set(0,0,0);
+  light3.position.set(-600, 0, 0);
+  light3.target.position.set(0,0,0);
+
+  function update() {
+    Controls.update();
+    if(!Controls.wasMoved()) earth.rotation.y += speed;
+    if(onRender) onRender();
+  }
+
+  function latLongToVector3(lat, lon, radius, heigth) {
+    var phi = (lat)*Math.PI/180;
+    var theta = (lon-180)*Math.PI/180;
+
+    var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
+    var y = (radius+heigth) * Math.sin(phi);
+    var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+
+    return new THREE.Vector3(x,y,z);
+  }
+
+  // Public API
+
+  instance.init = function(mapImg, rotationSpeed, options) {
+    if(!options) options = {};
+
+    speed = rotationSpeed;
+    onRender = options.onRender;
+
+    World.init({
+      clearColor: (options.bgColor === undefined ? 0xffffff : options.bgColor),
+      camDistance: 2500,
+      farPlane: 6000,
+      ambientLight: 0x111111,
+      renderCallback: update,
+      container: options.container
+    });
+
+    anchor.add(World.getCamera());
+    World.add(anchor);
+
+    var planetTexture = THREE.ImageUtils.loadTexture( mapImg );
+    var mat =  new THREE.MeshPhongMaterial( {
+          map: planetTexture,
+          color: 0x888888,
+          shininess: 0.8,
+          transparent: options.transparent || false,
+          side: THREE.FrontSide
+        });
+
+    earth = new THREE.Mesh(earthGeo, mat);
+
+    Controls.init(World.getCamera(), earth, 1200, false, false);
+
+    // Artifacts with THREE.DoubleSide and transparent materials have ugly artifacts
+    // so we cheat by having 2 meshes, one with FrontSide, one with BackSide
+    if(options.transparent) {
+      var mat2 =  new THREE.MeshPhongMaterial( {
+            map: planetTexture,
+            color: 0xffffff,
+            shininess: 0.8,
+            transparent: true,
+            opacity: 1.0,
+            side: THREE.BackSide
+      }),
+      earthInner = new THREE.Mesh(earthGeo, mat2);
+      earth.add(earthInner);
+    }
+
+    World.add(earth);
+    World.startRenderLoop();
+  }
+
+  instance.add = function(lat, lng, height, markerColor) {
+    var newMarker = new THREE.Mesh(marker, new THREE.MeshBasicMaterial({color: markerColor}));
+    newMarker.scale.set(1, 1, height);
+
+    var pos = latLongToVector3(lat, lng, 600, height / 2);
+    newMarker.position.set(pos.x, pos.y, pos.z);
+    newMarker.lookAt(origin);
+    earth.add(newMarker);
+
+    return newMarker;
+  }
+
+  instance.remove = function(marker) {
+    earth.remove(marker);
+  }
+
+  // For non-browserify
+  if(window.compat) {
+    console.log("Compat mode");
+    window.Globe = instance;
+  }
+
+  return instance;
+})();
